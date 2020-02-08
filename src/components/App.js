23e3af8cd6efea1f18ade06d4e437f012a26b1ed
Saulo Reis <sauloarth@gmail.com>
2020-02-08 01:35:06 -0300

    Adiciona funcionalidades relacionadas a grupos.

--- src/components/App.js
+++ src/components/App.js
@@ -11,9 +11,18 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      roomId: null,
       messages: [],
+      joinableRooms: [],
+      joinedRooms: [],
     };
+    this.sendMessage = this.sendMessage.bind(this);
+    this.subscribeToRoom = this.subscribeToRoom.bind(this);
+    this.getRooms = this.getRooms.bind(this);
+    this.selectRoom = this.selectRoom.bind(this);
+    this.leaveRoom = this.leaveRoom.bind(this);
   }
+
   componentDidMount() {
     const chatManager = new ChatManager({
       instanceLocator,
@@ -27,34 +36,96 @@ class App extends React.Component {
       .connect()
       .then((currentUser) => {
         console.log('Successful connection, ', currentUser);
-        return currentUser;
-      })
-      .then((currentUser) => {
-        currentUser.subscribeToRoomMultipart({
-          roomId: 'df592d48-aed9-461d-81cd-6ec366ea1284',
-          hooks: {
-            onMessage: (message) => {
-              this.setState({
-                messages: [...this.state.messages, message],
-              });
-            },
-            onAddedToRoom: (room) => {
-              console.log('Added to room ${room.name}');
-            },
-          },
-        });
+        this.currentUser = currentUser;
+        this.getRooms();
       })
       .catch((err) => {
         console.log('Error on connection: ', err);
       });
   }
 
+  sendMessage(message) {
+    this.currentUser
+      .sendSimpleMessage({
+        roomId: this.state.roomId,
+        text: message,
+      })
+      .catch((err) => {
+        console.log(`Error sending a message: ${err}`);
+      });
+  }
+
+  getRooms() {
+    this.currentUser
+    .getJoinableRooms()
+      .then((joinableRooms) => {
+        this.setState({
+          joinableRooms,
+          joinedRooms: this.currentUser.rooms,
+        });
+        console.log('state now: ', this.state);
+      })
+      .catch((err) => {
+        console.log('Error on retriving joined / joinable rooms: ', err);
+      });
+  }
+
+  subscribeToRoom(roomId) {
+    this.currentUser
+      .subscribeToRoomMultipart({
+        roomId,
+        hooks: {
+          onMessage: (message) => {
+            this.setState({
+              messages: [...this.state.messages, message],
+            });
+          },
+          onAddedToRoom: (room) => {
+            console.log(`Added to room ${room.name}`);
+          },
+        },
+      })
+      .then(room => {
+        this.setState({
+          roomId: room.id
+        });
+
+        this.getRooms();
+      })
+      .catch((err) => {
+        console.log('Error on subscribing room: ', err);
+      });
+  }
+
+  selectRoom(roomId) { 
+    this.setState({roomId});
+    this.getRooms();
+  }
+
+  leaveRoom(roomId) {
+    this.currentUser.leaveRoom({roomId})
+      .then(room => console.log(`Left room ${room.id}.`))
+      .catch(err => console.log(`Error leaving room ${roomId} : ${err}`));
+    this.getRooms();
+
+    const nextRoom = (this.state.joinedRooms.length > 0) && this.state.joinedRooms[0]; 
+    nextRoom && this.selectRoom(nextRoom.id);
+    this.getRooms();
+  }
+
   render() {
     return (
       <div className="app">
-        <RoomList />
+        <RoomList
+          joinableRooms = {this.state.joinableRooms}
+          joinedRooms = {this.state.joinedRooms}
+          subscribeToRoom = {this.subscribeToRoom}
+          activeRoom = {this.state.roomId}
+          selectRoom = {this.selectRoom}
+          leaveRoom = {this.leaveRoom}
+        />
         <MessageList messages={this.state.messages} />
-        <SendMessageForm />
+        <SendMessageForm sendMessage={this.sendMessage} />
         <NewRoomForm />
       </div>
     );
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 class RoomList extends React.Component {
   render() {
-    return <div className="rooms-list">Room List</div>;
+    const joinedRooms = this.props.joinedRooms;
+    const joinableRooms = this.props.joinableRooms;
+
+    return (
+      <div className="rooms-list">
+        <ul>
+          <h3>Subscribed Rooms:</h3>
+          {(joinedRooms.length > 0) && this.props.joinedRooms.map(room => {
+            return (
+              <li key={room.id} className="room" >
+                <a href="#" onClick={() => this.props.selectRoom(room.id)}># {room.name}</a>
+                <a href="#" onClick={() => this.props.leaveRoom(room.id)}> X</a>
+              </li>
+            )
+          })}
+        </ul>
+        <ul>
+          <h3>Join also:</h3>
+          {(joinableRooms.length > 0) && this.props.joinableRooms.map(room => {
+            return (
+              <li key={room.id} className="room">
+                <a href="#" onClick={() => this.props.subscribeToRoom(room.id)}># {room.name}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    );
   }
 }
 
