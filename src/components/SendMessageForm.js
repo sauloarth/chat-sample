f5dcd0220ebe63a4fe277b53abcc07299a640c4b
Saulo Reis <sauloarth@gmail.com>
2020-02-08 01:33:41 -0300

    Implementando envio de mensagem ao grupo.

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,8 +1,39 @@
 import React from 'react';
 
 class SendMessageForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      message: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.sendMessage(this.state.message);
+    this.setState({
+      message: '',
+    });
+  }
+
   render() {
-    return <div className="send-message-form">Send Message Form</div>;
+    return (
+      <form onSubmit={this.handleSubmit} className="send-message-form">
+        <input
+          placeholder="Type here and press Enter"
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.message}></input>
+      </form>
+    );
   }
 }
 
